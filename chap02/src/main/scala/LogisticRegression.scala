94915caab399355d1ef6638e8dc7ffa7fc562752
barmanroys <swagatopablo@aol.com>
2023-03-28 17:10:53 +0800

    Minor update

--- chap02/src/main/scala/LogisticRegression.scala
+++ chap02/src/main/scala/LogisticRegression.scala
@@ -4,34 +4,25 @@ import breeze.numerics._
 import breeze.optimize._
 
 class LogisticRegression(
-  val training:DenseMatrix[Double],
-  val target:DenseVector[Double]
-)
-{
-  private def costFunctionAndGradient(coefficients:DenseVector[Double])
-  :(Double, DenseVector[Double]) =
-  {
-      val xBeta = training * coefficients
-      val expXBeta = exp(xBeta)
-      val cost = - sum((target :* xBeta) - log1p(expXBeta))
-      val probabilities = sigmoid(xBeta)
-      val grad = training.t * (probabilities - target)
-      (cost, grad)
-  }  
+                          val training: DenseMatrix[Double],
+                          val target: DenseVector[Double]
+                        ) {
+  lazy val optimalCoefficient: DenseVector[Double] = calculateOptimalCoefficients
 
-  lazy val optimalCoefficient:DenseVector[Double] = calculateOptimalCoefficients
-
-
-
-
-  private def calculateOptimalCoefficients:DenseVector[Double] =
-  {
-      val f:DiffFunction[DenseVector[Double]] = new DiffFunction[DenseVector[Double]] {
-        def calculate(parameters:DenseVector[Double]) = 
-          costFunctionAndGradient(parameters)
-      }
-
-      minimize(f, DenseVector.zeros[Double](training.cols))
+  private def calculateOptimalCoefficients: DenseVector[Double] = {
+    val f: DiffFunction[DenseVector[Double]] = new DiffFunction[DenseVector[Double]] {
+      def calculate(parameters: DenseVector[Double]): (Double, DenseVector[Double]) =
+        costFunctionAndGradient(parameters)
+    }
+    minimize(f, DenseVector.zeros[Double](training.cols))
   }
 
+  private def costFunctionAndGradient(coefficients: DenseVector[Double]): (Double, DenseVector[Double]) = {
+    val xBeta = training * coefficients
+    val expXBeta = exp(xBeta)
+    val cost = -sum((target :* xBeta) - log1p(expXBeta))
+    val probabilities = sigmoid(xBeta)
+    val grad = training.t * (probabilities - target)
+    (cost, grad)
+  }
 }
--- chap02/src/main/scala/LogisticRegressionDemo.scala
+++ chap02/src/main/scala/LogisticRegressionDemo.scala
@@ -1,7 +1,7 @@
 
 object LogisticRegressionDemo {
 
-  def main(args:Array[String]) {
+  def main(args: Array[String]): Unit = {
     val data = HWData.load() // Method call without arguments can work without the parentheses
     //noinspection SpellCheckingInspection
     val regressor = new LogisticRegression(data.featureMatrix, data.target)
