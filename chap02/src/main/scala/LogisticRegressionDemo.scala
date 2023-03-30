697644be8da4f299e911570e795e81d7683dc22f
barmanroys <swagatopablo@aol.com>
2023-03-30 10:35:13 +0800

    Reverted to manually definining main method

--- chap02/src/main/scala/LogisticRegressionDemo.scala
+++ chap02/src/main/scala/LogisticRegressionDemo.scala
@@ -1,11 +1,14 @@
 
-object LogisticRegressionDemo extends App {
-  private val data = HWData.load() // Method call without arguments can work without the parentheses
-  //noinspection SpellCheckingInspection
-  private val regressor = new LogisticRegression(data.featureMatrix, data.target)
-  private val coefficients = regressor.calculateOptimalCoefficients()
-  println("Optimal coefficients (on re-scaled data): ")
-  println(coefficients)
+object LogisticRegressionDemo {
+  /** Unable to extend App here, and must manually define the main method as some SBT commands and spark clusters are incompatible with app */
+  def main(args: Array[String]): Unit = {
+    val data = HWData.load() // Method call without arguments can work without the parentheses
+    //noinspection SpellCheckingInspection
+    val regressor = new LogisticRegression(data.featureMatrix, data.target)
+    val coefficients = regressor.calculateOptimalCoefficients()
+    println("Optimal coefficients (on re-scaled data): ")
+    println(coefficients)
+  }
 }
 
 
