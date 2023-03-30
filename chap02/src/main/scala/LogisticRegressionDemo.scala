f641c574d069ebbc61ff4669620eb3574b3c17bf
barmanroys <swagatopablo@aol.com>
2023-03-30 09:05:21 +0800

    Main method replaced by subclassing from App  On branch master  Your branch is ahead of 'origin/master' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   src/main/scala/LogisticRegressionDemo.scala

--- chap02/src/main/scala/LogisticRegressionDemo.scala
+++ chap02/src/main/scala/LogisticRegressionDemo.scala
@@ -1,13 +1,11 @@
 
-object LogisticRegressionDemo {
-
-  def main(args: Array[String]): Unit = {
-    val data = HWData.load() // Method call without arguments can work without the parentheses
-    //noinspection SpellCheckingInspection
-    val regressor = new LogisticRegression(data.featureMatrix, data.target)
-    val coefficients = regressor.calculateOptimalCoefficients()
-    println("Optimal coefficients (on re-scaled data): ")
-    println(coefficients)
-  }
+object LogisticRegressionDemo extends App {
+  private val data = HWData.load() // Method call without arguments can work without the parentheses
+  //noinspection SpellCheckingInspection
+  private val regressor = new LogisticRegression(data.featureMatrix, data.target)
+  private val coefficients = regressor.calculateOptimalCoefficients()
+  println("Optimal coefficients (on re-scaled data): ")
+  println(coefficients)
 }
+
 
