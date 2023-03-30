0e79b59fcad53a1f6c98b016126c8c3d2af1f193
barmanroys <swagatopablo@aol.com>
2023-03-30 10:54:38 +0800

    Echo the working directory  Changes to be committed: 	modified:   src/main/scala/LogisticRegressionDemo.scala

--- chap02/src/main/scala/LogisticRegressionDemo.scala
+++ chap02/src/main/scala/LogisticRegressionDemo.scala
@@ -1,11 +1,14 @@
+import breeze.linalg.DenseVector
 
 object LogisticRegressionDemo {
   /** Unable to extend App here, and must manually define the main method as some SBT commands and spark clusters are incompatible with app */
   def main(args: Array[String]): Unit = {
-    val data = HWData.load() // Method call without arguments can work without the parentheses
+    val data: HWData = HWData.load() // Method call without arguments works with or without the parentheses
     //noinspection SpellCheckingInspection
     val regressor = new LogisticRegression(data.featureMatrix, data.target)
-    val coefficients = regressor.calculateOptimalCoefficients()
+    val coefficients: DenseVector[Double] = regressor.calculateOptimalCoefficients()
+    val directory: String = System.getProperty("user.dir") // Gives the build.sbt directory, not the scala source.
+    println(s"The current working directory is: $directory.")
     println("Optimal coefficients (on re-scaled data): ")
     println(coefficients)
   }
